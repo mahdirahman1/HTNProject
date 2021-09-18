d3ad7c382810d103f61eac3e47e4484a9ab79a0f
Raihaan <raihaan129@gmail.com>
2021-09-18 14:02:19 -0400

    delete about.jsx

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Subcomponent from './Subcomponent';
+import { Route, Switch } from 'react-router-dom';
 
 const Join = (props) => (
   <div className='' style={{ color: 'white' }}>
@@ -7,6 +8,7 @@ const Join = (props) => (
     <Subcomponent event={'event420'} time={"5 AM"} date={'Sep 18 2021'} attendees={123}/>
     <Subcomponent event={'event69'} time={"12 PM"}date={'Dec 25 3012'} attendees={456}/>
     <Subcomponent event={'loltest'} time={"8 PM"}date={'Jan 10 1765'} attendees={456}/>
+
   </div>
 )
 
