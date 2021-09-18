d9c55c7ee5471e5475b854e23b3ddd2c71f6e22b
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2021-09-18 16:47:17 -0400

    event handlers for login/sign up

--- src/components/forms/SignupForm.jsx
+++ src/components/forms/SignupForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.css";
 import Form from "react-bootstrap/Form";
@@ -9,19 +9,47 @@ const FormGroup = styled(Form.Group)`
 `;
 
 const SignupForm = () => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const submitHandler = () => {
+    alert(`${email} ${password}`);
+  };
+
+  const onChangeHandlePassword = (e) => {
+    setpassword(e.target.value);
+  };
+  const onChangeHandleEmail = (e) => {
+    setemail(e.target.value);
+  };
+  const onChangeHandleName = (e) => {
+    setname(e.target.value);
+  };
   return (
-    <Form>
+    <Form onSubmit={submitHandler}>
       <FormGroup>
         <Form.Label style={{ color: "white" }}>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter your nanme" />
+        <Form.Control
+          type="text"
+          placeholder="Enter your nanme"
+          onChange={onChangeHandleName}
+        />
       </FormGroup>
       <FormGroup>
         <Form.Label style={{ color: "white" }}>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter your email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter your email"
+          onChange={onChangeHandleEmail}
+        />
       </FormGroup>
       <FormGroup>
         <Form.Label style={{ color: "white" }}>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter your your password" />
+        <Form.Control
+          type="password"
+          placeholder="Enter your your password"
+          onChange={onChangeHandlePassword}
+        />
       </FormGroup>
       <FormGroup>
         <Button style={{ width: "100%" }} variant="primary" type="submit">
